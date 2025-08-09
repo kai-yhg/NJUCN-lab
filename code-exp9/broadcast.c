dfa49f02ad622e5bd080b23154c09dd71c439c09
zeyu zhang <zzy1410055395@163.com>
2025-08-09 22:59:52 +0800

    Add files via upload

---
+++ code-exp9/broadcast.c
@@ -0,0 +1,21 @@
+#include "base.h"
+#include <stdio.h>
+#include <log.h>
+// XXX ifaces are stored in instace->iface_list
+extern ustack_t *instance;
+
+extern void iface_send_packet(iface_info_t *iface, const char *packet, int len);
+
+void broadcast_packet(iface_info_t *iface, const char *packet, int len)
+{
+	// TODO: broadcast packet
+	// fprintf(stdout, "TODO: broadcast packet.\n");
+	iface_info_t *iface_entry = NULL;
+	list_for_each_entry(iface_entry, &instance->iface_list, list)
+	{
+		if (iface_entry && iface_entry != iface)
+		{
+			iface_send_packet(iface_entry, packet, len);
+		}
+	}
+}
